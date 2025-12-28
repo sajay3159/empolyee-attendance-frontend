cbd44294dfcccb7cc67bb11770f868c1ef16ef9d
Ajay Sahani <sajay3159@gmail.com>
2025-12-28 08:17:05 +0530

    update axios

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://employee-attendance-backend-v3r5.onrender.com/api",
+  baseURL: import.meta.env.VITE_API_BASE_URL,
   headers: {
     "Content-Type": "application/json",
   },
