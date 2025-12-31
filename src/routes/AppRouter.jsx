f0bb37fc57238836f21dfde8e5b1fe190e60a1f4
Ajay Sahani <sajay3159@gmail.com>
2025-12-31 14:55:54 +0530

    implement React.lazy for route-based code splitting

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -1,57 +1,62 @@
 import { Routes, Route } from "react-router-dom";
+import { Suspense, lazy } from "react";
 import Login from "../pages/Login";
-import Dashboard from "../pages/Dashboard";
-import Attendance from "../pages/Attendance";
 import ProtectedRoute from "./ProtectedRoute";
 import PublicRoute from "./PublicRoute";
 import AppLayout from "../components/layout/AppLayout";
-import EmployeeAttendance from "../pages/EmployeeAttendance";
-import AddEmployee from "../pages/AddEmployee";
+
+const Dashboard = lazy(() => import("../pages/Dashboard"));
+const Attendance = lazy(() => import("../pages/Attendance"));
+const EmployeeAttendance = lazy(() => import("../pages/EmployeeAttendance"));
+const AddEmployee = lazy(() => import("../pages/AddEmployee"));
 
 const AppRouter = () => {
   return (
-    <Routes>
-      <Route
-        path="/login"
-        element={
-          <PublicRoute>
-            <Login />
-          </PublicRoute>
-        }
-      />
+    <Suspense fallback={<div>Loading...</div>}>
+      <Routes>
+        <Route
+          path="/login"
+          element={
+            <PublicRoute>
+              <Login />
+            </PublicRoute>
+          }
+        />
 
-      <Route
-        element={
-          <ProtectedRoute roles={["admin"]}>
-            <AppLayout />
-          </ProtectedRoute>
-        }
-      >
-        <Route path="/" element={<Dashboard />} />
-        <Route path="/attendance" element={<Attendance />} />
-      </Route>
-      <Route
-        path="/employee"
-        element={
-          <ProtectedRoute roles={["employee"]}>
-            <AppLayout />
-          </ProtectedRoute>
-        }
-      >
-        <Route index element={<EmployeeAttendance />} />
-      </Route>
+        <Route
+          element={
+            <ProtectedRoute roles={["admin"]}>
+              <AppLayout />
+            </ProtectedRoute>
+          }
+        >
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/attendance" element={<Attendance />} />
+        </Route>
 
-      <Route
-        path="/add-employee"
-        element={
-          <ProtectedRoute roles={["admin"]}>
-            <AppLayout />
-          </ProtectedRoute>
-        }
-      >
-        <Route index element={<AddEmployee />} />
-      </Route>
-    </Routes>
+        <Route
+          path="/employee"
+          element={
+            <ProtectedRoute roles={["employee"]}>
+              <AppLayout />
+            </ProtectedRoute>
+          }
+        >
+          <Route index element={<EmployeeAttendance />} />
+        </Route>
+
+        <Route
+          path="/add-employee"
+          element={
+            <ProtectedRoute roles={["admin"]}>
+              <AppLayout />
+            </ProtectedRoute>
+          }
+        >
+          <Route index element={<AddEmployee />} />
+        </Route>
+      </Routes>
+    </Suspense>
   );
 };
 
